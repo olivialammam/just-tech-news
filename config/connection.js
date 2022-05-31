a84ef99e06feb7e0dd78835305e062e991e5dfbb
Olivia Lammam <olivia.l7475@gmail.com>
2022-05-30 21:13:12 -0700

    handlebars

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,7 @@
-// import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -11,14 +14,41 @@ if (process.env.JAWSDB_URL) {
   });
 }
 
-require('dotenv').config();
-
-
-// create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
-
 module.exports = sequelize;
+
+// // create connection to our db
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306
+//     });
+
+
+
+
+// // import the Sequelize constructor from the library
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+//   });
+// }
+
+// require('dotenv').config();
+
+
+// // create connection to our database, pass in your MySQL information for username and password
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   port: 3306
+// });
+
+// module.exports = sequelize;
